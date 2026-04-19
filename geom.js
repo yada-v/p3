600a05f9afa1bb467e06feacba6fcc0bb426be86
yada-v <yadavkraj555@gmail.com>
2026-04-19 10:51:18 +0530

    Update geom.js

--- geom.js
+++ geom.js
@@ -88,6 +88,8 @@ text('mg', - sin(PI/15)*Lscale, cy + Lscale);
 text('Angular momentum',cx,cy - Gscale);
 text('Torque',cx + Tscale*2,cy);
 let j = -sin(PI/15)*height/20;
+textAlign(BOTTOM,CENTER);
+textSize(12);
 text('d',j,6);
 
 pop();
